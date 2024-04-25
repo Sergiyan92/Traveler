3adacbda38c11fdaddceb26c14367df878bac77f
Sergiyan92 <sergiyan@ukr.net>
2024-04-25 10:34:27 +0300

    learn v-if && v-else

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script setup>
-import IBotton from './components/IButton/IBotton.vue'
+import HomepageView from './views/HomePageView.vue'
+const isDesktop = window.innerWidth > 1024
+const isTablet = window.innerWidth <= 1024 && window.innerWidth > 748
 </script>
 <template>
-  <IBotton></IBotton>
+  <template v-if="isDesktop">
+    <HomepageView />
+  </template>
+  <template v-else-if="isTablet">
+    <div>This is tablet</div>
+  </template>
+  <template v-else>
+    <div v-show="!isDesktop && !isTablet">This is mobile</div>
+  </template>
+  <div v-show="!isDesktop && !isTablet">This is mobile</div>
 </template>
